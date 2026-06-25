9eb8408232c840d28503ffdd38ffe045b47b4d96
Joao Carnevalli <joaovictor.carnevalli@gmail.com>
2026-06-25 20:19:43 -0300

    nova atualizacao

---
+++ 4 - Iniciando JS/1-variaveis.js
@@ -0,0 +1,21 @@
+/* CRIAÇÃO DE VARIAVEIS */
+
+
+/* VAR aceita qualquer tipo de valor(string, numero)*/
+var curso = "Javascript";
+
+
+/* Para utilizar a variavel em um alerta */
+alert(curso);
+alert(curso+' Sujeito Programador');
+
+
+/* É POSSIVEL SOMAR NUMEROS UTILIZANDO AS VARIAVEIS */
+
+var valor1 = 7;
+var valor2 = 8;
+var soma = valor1 + valor2;
+
+/* Variveis sao case sensitives, ou seja, o nome deve ser exatamente igual */
+
+
---
+++ 4 - Iniciando JS/2-script.js
@@ -0,0 +1,9 @@
+var nome = prompt("Digite seu nome: ");
+
+var sobrenome = prompt("Digite seu sobrenome: ");
+
+console.log("Olá, "+nome+" "+sobrenome);
+
+/* Document se trata da nossa pagina em si */
+document.write("<h1> Bem vindo " +nome+" "+sobrenome+" ao projeto </h1>")
+document.write("<img src='https://sujeitoprogramador.com/steve.png'") 
