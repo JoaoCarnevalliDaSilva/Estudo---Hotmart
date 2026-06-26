68403ffa11f32a9c954dc4bcbaea69a2a134ea60
Joao Carnevalli <joaovictor.carnevalli@gmail.com>
2026-06-25 21:05:47 -0300

    ultima atualizacao de hoje

---
+++ 4 - Iniciando JS/4-listas.js
@@ -0,0 +1,12 @@
+/* Criando uma lista que eu ja determino os itens da lista*/
+var lista = ['Matheus', 'Lucas', 'Jose', 150];
+
+lista.indexOf('Jose'); /* Devolve a posicao do elemento procurado */
+
+lista.push('Carlos'); /* Adicionando itens a lista */
+
+lista.shift(); /* Remove o primeiro item da lista */
+
+lista.pop(); /* Remove o ultimo item da lista */
+
+lista.join(';'); /* Separa os itens de acordo com o caracter em () */
