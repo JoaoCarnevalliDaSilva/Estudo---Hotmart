073c29f2378b80a082296ac72b36c4ebe3b803f3
Joao Carnevalli <joaovictor.carnevalli@gmail.com>
2026-07-04 16:02:43 -0300

    nova atualizacao

--- 00 - Praticas Aleatorias/6 - script.js
+++ 00 - Praticas Aleatorias/6 - script.js
@@ -9,7 +9,7 @@ function calcular(event) {
   let economia = 0;
   economia = gastoTradicional - gastoHibrido;
   const resultado = document.getElementById('resultado');
-  if(economia => 300) {
+  if(economia >= 300) {
     resultado.innerHTML = "Uau! Você vai economizar R$ "+economia.toFixed(2)+" por mês! Tá na hora de trocar de carro!";
   } else if (economia > 0) {
     resultado.innerHTML = "Você economizará R$ "+economia.toFixed(2)+" . Uma ótima ajuda no fim do mês!";
