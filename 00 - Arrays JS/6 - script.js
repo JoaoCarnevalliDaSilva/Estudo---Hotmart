ab107a841a7b147c462fc3228360c96b9839a18f
Joao Carnevalli <joaovictor.carnevalli@gmail.com>
2026-07-07 20:45:45 -0300

    nova

--- 00 - Arrays JS/6 - script.js
+++ 00 - Arrays JS/6 - script.js
@@ -2,18 +2,39 @@ let grade = [];
 
 function adicionar(event) {
   event.preventDefault();
-  const adicaoTemp = document.getElementById('adicione').value;
-  const resultadoErro = document.getElementById('resultado-erro').value;
+  const adicaoTemp = document.getElementById('input-adicionar').value;
+  const resultadoErro = document.getElementById('resultado-erro');
   const resultado = document.getElementById('resultado');
-  grade.push(adicaoTemp);
-  if(grade.includes(adicaoTemp)) {    
-    resultadoErro.innerHTML = "Atenção: Essa matéria já está no seu cronograma!";
-    document.getElementById('adicione').value='';
+  if(grade.includes(adicaoTemp)) {   
+    resultado.innerHTML = ''; 
+    resultadoErro.innerHTML = "Atenção: Essa tópico já está na sua lista!";
+    document.getElementById('input-adicionar').value='';
   } else {
     resultadoErro.innerHTML = '';
     grade.push(adicaoTemp);
     resultado.innerHTML = "Adicionado com sucesso.";
-    document.getElementById('adicione').value='';
+    document.getElementById('input-adicionar').value='';
   }
+  const demonstrarLista = document.getElementById('lista-completa');
+  demonstrarLista.innerHTML = "Sua lista atual é: "+grade.join(' - ');
+}
+
+function finalizar(event) {
+
+}
+
+function deletar(event) {
+  event.preventDefault();
+  const adicaoTemp = document.getElementById('input-finalizar').value;
+  const posicao = grade.indexOf(adicaoTemp);
+  const resultado = document.getElementById('resultado');
+  if(posicao !== -1) {
+    grade.splice(posicao,1);
+    resultado.innerHTML = "Último tópico removido com sucesso."
+  } else {
+    resultado.innerHTML = "Tópico não encontrado"
+  }
+  const demonstrarLista = document.getElementById('lista-completa');
+  demonstrarLista.innerHTML = "Sua lista atual é: "+grade.join(' - ');
 }
 
