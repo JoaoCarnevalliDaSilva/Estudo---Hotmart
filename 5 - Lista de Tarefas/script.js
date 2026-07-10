24044e52ae0a2d456b2f4ec898a9c2bfc1472ea6
Joao Carnevalli <joaovictor.carnevalli@gmail.com>
2026-07-09 21:26:59 -0300

    atualiza

---
+++ 5 - Lista de Tarefas/script.js
@@ -0,0 +1,27 @@
+let listElement = document.querySelector("#app ul");
+let inputElement = document.querySelector("#app input");
+let buttonElement = document.querySelector("#app button");
+
+let tarefas = [];
+
+function renderTarefas() {
+  listElement.innerHTML = '';
+  tarefas.map((todo)=>{
+
+
+  })
+}
+
+
+function adicionarTarefas() {
+  if(inputElement.value === '') {
+    alert("Digite alguma tarefa");
+    return false;
+  } else {
+    let novaTarefa = inputElement.value;
+    tarefas.push(novaTarefa);
+    inputElement.value = '';
+  }
+}
+
+buttonElement.onclick = adicionarTarefas;
