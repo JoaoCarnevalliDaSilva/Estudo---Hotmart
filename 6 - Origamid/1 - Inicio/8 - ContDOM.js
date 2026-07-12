f9d366b20e855824d932b3c929905590bd2d67ca
Joao Carnevalli <joaovictor.carnevalli@gmail.com>
2026-07-12 17:39:28 -0300

    ultima de hoje

--- 6 - Origamid/1 - Inicio/8 - ContDOM.js
+++ 6 - Origamid/1 - Inicio/8 - ContDOM.js
@@ -25,9 +25,6 @@ const imgAnimais = document.querySelectorAll('.animais img'); //Tags img dentro 
 // O valor não atualiza se houver a inserção de novos itens
 
 
-
-
-
 const gridSectionHTML = document.getElementsByClassName('grid-section'); 
 const gridSectionNode = document.querySelectorAll('.grid-section');
 
@@ -40,3 +37,33 @@ gridSectionNode.forEach(function(item, index){
   console.log(item);
 })
 //Não é possivel fazer isso com o get elements
+
+
+
+// -----------------  EXERCICIOS  ------------------------
+
+//Retorne no console todas as imagens do site:
+
+const img = document.querySelectorAll('img');
+console.log(img);
+
+//Retorno no console apenas as imagens que começaram com a palavra imagem
+
+const comecaImagem = document.querySelectorAll('img[src^="img/imagem"]');
+console.log(comecaImagem)
+
+//Selecione todos os links internos (onde o href começa com o #)
+
+const linkInterno = document.querySelectorAll('[href^="#"]');
+console.log(linkInterno);
+
+
+//Selecione o primeiro h2 dentro de .animais-descricao
+
+const h2Animais = document.querySelector('.animais-descricao h2');
+console.log(h2Animais);
+
+//Selecione o ultimo p do site
+
+const paragrafos = document.querySelectorAll('p');
+console.log(paragrafos[paragrafos.length - 1]);
