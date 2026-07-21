0dde7bc050be98d91151a6c80c60f593a684bbd5
Joao Carnevalli <joaovictor.carnevalli@gmail.com>
2026-07-20 21:21:27 -0300

    atualiza

--- 00 - Projeto JS/script.js
+++ 00 - Projeto JS/script.js
@@ -2,3 +2,18 @@ const usuario_mock =  {
   email: "coordenador@sanquim.com",
   senha: "senha123"
 };
+
+const formulario = document.querySelector('#login-form');
+const inputEmail = document.querySelector('#email');
+const inputSenha = document.querySelector('#senha');
+
+formulario.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const emailDigitado = inputEmail.value;
+  const senhaDigitado = inputSenha.value;
+  if (emailDigitado === usuario_mock.email && senhaDigitado === usuario_mock.senha) {
+    alert('Acesso liberado');
+  } else {
+    alert('Acesso Negado');
+  }
+})
