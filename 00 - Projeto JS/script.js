40cba146ff657be2afbe2bf39e793d05ce8e4d31
Joao Carnevalli <joaovictor.carnevalli@gmail.com>
2026-07-20 22:13:24 -0300

    atualiza

--- 00 - Projeto JS/script.js
+++ 00 - Projeto JS/script.js
@@ -7,12 +7,16 @@ const formulario = document.querySelector('#login-form');
 const inputEmail = document.querySelector('#email');
 const inputSenha = document.querySelector('#senha');
 
+const telaLogin = document.querySelector('#tela-login');
+const telaDashboard = document.querySelector('#dashboard');
+
 formulario.addEventListener('submit', function(event) {
   event.preventDefault();
   const emailDigitado = inputEmail.value;
   const senhaDigitado = inputSenha.value;
   if (emailDigitado === usuario_mock.email && senhaDigitado === usuario_mock.senha) {
-    alert('Acesso liberado');
+    telaLogin.style.display = 'none' //Oculta tela inicial
+    telaDashboard.style.display = 'block'; //Revela painel coordenador
   } else {
     alert('Acesso Negado');
   }
