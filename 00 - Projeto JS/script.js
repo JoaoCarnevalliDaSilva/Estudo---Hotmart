c32740602750142626d3099e86c4e30d76bc413c
Joao Carnevalli <joaovictor.carnevalli@gmail.com>
2026-07-21 22:49:50 -0300

    nova atualização

--- 00 - Projeto JS/script.js
+++ 00 - Projeto JS/script.js
@@ -1,4 +1,4 @@
-const usuario_mock =  {
+const usuario_mock = {
   email: "coordenador@sanquim.com",
   senha: "senha123"
 };
@@ -12,12 +12,19 @@ const telaDashboard = document.querySelector('#dashboard');
 
 formulario.addEventListener('submit', function(event) {
   event.preventDefault();
+  
   const emailDigitado = inputEmail.value;
   const senhaDigitado = inputSenha.value;
+  
   if (emailDigitado === usuario_mock.email && senhaDigitado === usuario_mock.senha) {
-    telaLogin.style.display = 'none' //Oculta tela inicial
-    telaDashboard.style.display = 'block'; //Revela painel coordenador
+    
+    // Oculta o login
+    telaLogin.style.display = 'none';
+    
+    // Revela o dashboard (usando 'grid' para não quebrar seu layout!)
+    telaDashboard.style.display = 'grid';
+    
   } else {
-    alert('Acesso Negado');
+    alert('Acesso Negado: E-mail ou senha incorretos.');
   }
-})
+});
