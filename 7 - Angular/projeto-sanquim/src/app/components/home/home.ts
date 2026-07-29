ba5507eade1351f9a1640d38630a06d972679213
Joao Carnevalli <joaovictor.carnevalli@gmail.com>
2026-07-28 22:23:26 -0300

    nova atualização

---
+++ 7 - Angular/projeto-sanquim/src/app/components/home/home.ts
@@ -0,0 +1,11 @@
+// CLASSE QUE VAI REPRESENTAR O COMPONENTE - ONDE FICA A LOGICA E TODA A DINAMICA DA PAGINA
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {}
