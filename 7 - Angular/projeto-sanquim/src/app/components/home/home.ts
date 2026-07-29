627de6361757ac553588430f45baeb99c72614ed
Joao Carnevalli <joaovictor.carnevalli@gmail.com>
2026-07-29 15:54:39 -0300

    atualiza

--- 7 - Angular/projeto-sanquim/src/app/components/home/home.ts
+++ 7 - Angular/projeto-sanquim/src/app/components/home/home.ts
@@ -2,27 +2,22 @@
 
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'; // Necessário para ler os inputs
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FormsModule], // Importamos o FormsModule aqui
+  imports: [CommonModule, FormsModule], 
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class HomeComponent {
-  // Variáveis que vão guardar o que for digitado nos inputs
   emailDigitado: string = '';
   senhaDigitada: string = '';
 
-  // Função que é acionada quando o usuário clica em Entrar
   fazerLogin() {
-    // Validação "mokada" (falsa/simulada)
     if (this.emailDigitado === 'admin@sanquim.com' && this.senhaDigitada === '123') {
       alert('Login realizado com sucesso! Bem-vindo ao Sistema Sanquim 🚀');
-      
-      // Aqui, daqui a pouco, nós vamos mandar o usuário para a tela do Dashboard!
       
     } else {
       alert('E-mail ou senha incorretos! Tente admin@sanquim.com / 123');
